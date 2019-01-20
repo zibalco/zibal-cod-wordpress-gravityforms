9b1b36d475fad600090034a98b7109504f588a68
mzamanzadeh <mohammad.zamanzadeh@gmail.com>
2019-01-20 16:46:05 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zibal-cod-gravityforms
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: پرداخت در محل زیبال گرویتی فرم
+Plugin URI: http://gravityforms.ir/
+Description: افزونه پرداخت در محل زیبال برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.3.1
+Author: گرویتی فرم پارسی و زیبال
+Author URI: http://gravityforms.ir/
+*/
+if (!defined('ABSPATH')) exit;
+require_once('zibalCod.php');
